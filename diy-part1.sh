dae2f8d001f8baa48238c0bf4649c2d3a25053d7
唯依 <46625336+onlyLTY@users.noreply.github.com>
2022-11-17 23:41:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@ sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-echo 'src-git kiddin9_Packages https://github.com/kiddin9/openwrt-packages.git' >> feeds.conf.default
+#echo 'src-git kiddin9_Packages https://github.com/kiddin9/openwrt-packages.git' >> feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
